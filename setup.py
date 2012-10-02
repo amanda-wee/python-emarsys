631b24c39ca86ff5c2789884c7bb93a77d9fd859
Eugene Wee <eugene@42ventures.com.sg>
2012-10-02 15:13:31 +0800

    Initial add of files.

---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+import emarsys
+
+CLASSIFIERS = ["Development Status :: 1 - Planning",
+               "Intended Audience :: Developers",
+               "License :: OSI Approved :: Apache Software License",
+               "Natural Language :: English",
+               "Operating System :: OS Independent",
+               "Programming Language :: Python",
+               "Topic :: Software Development :: Libraries :: Python Modules"]
+
+KEYWORDS = "emarsys api wrapper"
+
+REPO_URL = "https://github.com/eugene-wee/python-emarsys"
+
+setup(name="python-emarsys",
+      version=emarsys.__version__,
+      description="""Emarsys REST API wrapper for Python.""",
+      author=emarsys.__author__,
+      url=REPO_URL,
+      packages=find_packages(),
+      download_url=REPO_URL + "/tarball/master",
+      classifiers=CLASSIFIERS,
+      keywords=KEYWORDS,
+      zip_safe=True,
+      install_requires=["distribute"])
